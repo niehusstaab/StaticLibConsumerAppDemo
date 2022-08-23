0386f20a7aa263cfe5cfdbb51800b47cfdef6834
honey-mobile-release <lnieuhusstaab@paypal.com>
2022-08-23 17:11:46 -0600

    add in demo static lib

--- StaticLibConsumerAppDemo/StaticLibConsumerAppDemo/ViewController.swift
+++ StaticLibConsumerAppDemo/StaticLibConsumerAppDemo/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import StaticLib
 
 class ViewController: UIViewController {
 
@@ -17,7 +18,7 @@ class ViewController: UIViewController {
   
   private func setup() {
     let tView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 100))
-    tView.text = "Hello \(getSomethingFromStaticLib())"
+    tView.text = "Hello: \(getSomethingFromStaticLib())"
     tView.textColor = UIColor.black
     tView.translatesAutoresizingMaskIntoConstraints = true
     view.addSubview(tView)
@@ -26,8 +27,14 @@ class ViewController: UIViewController {
     tView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
   }
 
-  private func getSomethingFromStaticLib() -> Int {
-    
+  private func getSomethingFromStaticLib() -> String {
+//    return StaticLibDemo().text // this works
+    let dic = StaticLibDemo().getResource()
+    return dic["title"]!!
+    /*
+    ^ This fails with following error:
+    2022-08-23 17:09:54.872641-0600 StaticLibConsumerAppDemo[44944:2189617] StaticLib/StaticLibDemo.swift:6: Fatal error: Unexpectedly found nil while unwrapping an Optional value
+     */
   }
 }
 
