c8798e603953da15be5ace13560865eb20977f13
honey-mobile-release <lnieuhusstaab@paypal.com>
2022-08-23 16:20:44 -0600

    setup for consuem static lib

--- StaticLibConsumerAppDemo/StaticLibConsumerAppDemo/ViewController.swift
+++ StaticLibConsumerAppDemo/StaticLibConsumerAppDemo/ViewController.swift
@@ -12,8 +12,22 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    setup()
+  }
+  
+  private func setup() {
+    let tView = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 100))
+    tView.text = "Hello \(getSomethingFromStaticLib())"
+    tView.textColor = UIColor.black
+    tView.translatesAutoresizingMaskIntoConstraints = true
+    view.addSubview(tView)
+    
+    tView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    tView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
   }
 
-
+  private func getSomethingFromStaticLib() -> Int {
+    
+  }
 }
 
